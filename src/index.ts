b0de961cb9dafae81ce6c2a6896d6f65c23648e8
TMN <tommymartin1234@gmail.com>
2024-05-19 10:32:35 -0600

    return json

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import { Elysia } from "elysia";
 import { cors } from '@elysiajs/cors'
 
 
-const app = new Elysia().use(cors()).get("/", () => "Hello World").listen(5000);
+const app = new Elysia().use(cors()).get("/", () => ({
+  hello: "world"
+})).listen(5000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
