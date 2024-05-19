e84d15b448081351a0a13c89333a69a30b5988ea
TMN <tommymartin1234@gmail.com>
2024-05-19 09:53:27 -0600

    feat: cors added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
+import { cors } from '@elysiajs/cors'
 
-const app = new Elysia().get("/", () => "Hello World").listen(5000);
+
+const app = new Elysia().use(cors()).get("/", () => "Hello World").listen(5000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
